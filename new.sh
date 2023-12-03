09e5200392c0985a071350366fd7b46825c51b8e
puttehi <peteeez95@gmail.com>
2023-12-03 16:04:14 +0200

    Add day dir generator

---
+++ new.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+# Initialize a new challenge
+
+set -euo pipefail
+
+DAY_NO=$1
+
+git checkout -b "$DAY_NO"
+mkdir -p "$DAY_NO"
+pushd "$DAY_NO"
+    cat <<EOF > README.md
+# Problem #$DAY_NO -- 2023-12-$DAY_NO
+
+https://adventofcode.com/2023/day/1
+
+## Problem statement
+
+...
+
+## Input
+
+https://adventofcode.com/2023/day/$DAY_NO/input
+
+## Requirements
+
+...
+
+## Run
+
+Build and run: \`make all\` or:
+
+1. Build: \`make\`
+2. Run:   \`make run\`
+EOF
+    cat <<EOF > Makefile
+SHELL:=/bin/bash
+
+all: build run
+
+build:
+    echo "build command"
+
+run:
+    echo "run command"
+EOF
+popd "$1"
